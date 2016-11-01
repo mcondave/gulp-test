71edc6f9a16a2e3104b84910a564817fa8688489
Mike <mcondave@gmail.com>
2016-11-01 12:00:54 -0400

    Finished optimization tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
 
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
@@ -19,7 +22,7 @@ gulp.task('browserSync', function() {
         server: {
 	    baseDir: 'app'
 	},
-    })
+    });
 });
 
 gulp.task('hello', function() {
@@ -32,7 +35,7 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('app/css')) // Outputs the file in the destination folder
     .pipe(browserSync.reload({
         stream: true
-    }))
+    }));
 });
 
 gulp.task('useref', function() {
@@ -40,5 +43,28 @@ gulp.task('useref', function() {
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+        // Setting interlaced to true
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+gulp.task('cache:clear', function(callback) {
+    return cache.clearAll(callback);
 });
